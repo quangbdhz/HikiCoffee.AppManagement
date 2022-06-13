019e6d2e03a4ae3177eb38493a5a373165ddc59c
Tran Quang <tranquangbdhz@gmail.com>
2022-06-13 15:07:48 +0700

    customer

---
+++ HikiCoffee.ApiIntegration/UserAPI/IUserAPI.cs
@@ -0,0 +1,12 @@
+﻿using HikiCoffee.Models;
+using HikiCoffee.Models.Common;
+
+namespace HikiCoffee.ApiIntegration.UserAPI
+{
+    public interface IUserAPI
+    {
+        Task<PagedResult<User>> GetAllUsers(int pageIndex, int pageSize, string? token);
+
+        Task<string> DeleteUser(Guid userId, string? token);
+    }
+}
---
+++ HikiCoffee.ApiIntegration/UserAPI/UserAPI.cs
@@ -0,0 +1,19 @@
+﻿using HikiCoffee.Models;
+using HikiCoffee.Models.Common;
+using HikiCoffee.Utilities;
+
+namespace HikiCoffee.ApiIntegration.UserAPI
+{
+    public class UserAPI : BaseAPI, IUserAPI
+    {
+        public async Task<string> DeleteUser(Guid userId, string? token)
+        {
+            return await DeleteAsync(SystemConstants.DomainName + $"/api/Users/Delete/{userId.ToString()}", token);
+        }
+
+        public async Task<PagedResult<User>> GetAllUsers(int pageIndex, int pageSize, string? token)
+        {
+            return await GetAsync<PagedResult<User>>(SystemConstants.DomainName + $"/api/Users/GetPagingUserManagements?PageIndex={pageIndex}&PageSize={pageSize}", token);
+        }
+    }
+}
